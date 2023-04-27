137b7a85826c8246451b05cdd094ca14e646a3ec
Teddy <teddy@numherit.com>
2023-04-27 16:47:43 +0300

    amelio

--- src/components/Residences/Residences.jsx
+++ src/components/Residences/Residences.jsx
@@ -1,6 +1,8 @@
 import {Swiper, useSwiper, SwiperSlide} from "swiper/react";
 import 'swiper/css'
 import data from '../../../src/utils/slider.json'
+import {color} from "framer-motion";
+import './Residences.css'
 
 function Residences() {
     return (
@@ -10,6 +12,26 @@ function Residences() {
                     <span className="orangeText">Meilleur choix</span>
                     <span className="primaryText">Résidence populaire</span>
                 </div>
+                <Swiper>
+                    {
+                        data.map((card, i) => (
+                            <SwiperSlide key={i}>
+                                <div className="flexColStart r-card">
+                                    <img src={card.image} alt="home"/>
+                                    <span className="secondaryText r-price">
+                                        <span style={{color: "orange"}}>$
+                                        </span>
+                                        <span>
+                                            {card.price}
+                                        </span>
+                                    </span>
+                                    <span className="primaryText">{card.name}</span>
+                                    <span className="secondaryText">{card.detail}</span>
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
             </div>
         </section>
     );
